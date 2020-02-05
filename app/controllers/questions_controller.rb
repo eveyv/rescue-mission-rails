cb8f5546d895b472c54733f12da08febb8d963d4
Everett Yeaw <evyeaw@gmail.com>
2020-02-05 11:02:46 -0500

    rescue-mission-rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,11 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.order('created_at DESC')
-    # @question = Question.new(params["question"])
   end
 
   def show
     @question = Question.find(params["id"])
     @answer = Answer.new
-  #
     @answers = @question.answers.order(created_at: :asc)
   end
 
@@ -26,6 +24,31 @@ class QuestionsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @question = Question.find(params["id"])
+  end
+
+  def update
+    @question = Question.find(params["id"])
+    @question.update(question_params)
+
+    if @question.save
+      flash[:notice] = "Question has been updated!"
+      redirect_to @question
+    else
+      flash.now[:notice]= @question.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params["id"])
+    @question.answers.destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+
 
   private
 
